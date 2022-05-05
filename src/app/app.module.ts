01bef15a8c846996fffe70f21857f443aadfe518
Manuel Hernández <carlosmanuel.hernandez@bosonit.com>
2022-05-05 10:40:13 +0200

    Feat: Basic Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule
